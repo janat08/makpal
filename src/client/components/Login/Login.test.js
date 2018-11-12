a4094f196a469b822ce8b6ba2200db305a81f6c8
zhan <jey.and.key@gmail.com>
2018-11-12 13:02:58 +0600

    jest

---
+++ src/client/components/Login/Login.test.js
@@ -0,0 +1,10 @@
+import Login from './Login';
+
+
+describe('pages/Home', function() {
+
+  it('should render content', function() {
+    expect(true).toEqual(true)
+  });
+
+});
--- test/unit/jest.unit-test.init.js
+++ test/unit/jest.unit-test.init.js
@@ -2,12 +2,12 @@
 
 import './jest.polyfills.js'; //polyfills must be imported before other imports to suppress jest warnings
 import { configure, shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 // import toJson from 'enzyme-to-json';
 
 //React Enzyme adapter
-configure({ adapter: new Adapter() });
+// configure({ adapter: new Adapter() });
 
 //expose common functions used in tests
 //yes, globals are bad, but; 
