1d4b9af63a07a03dbdd9e9a053cc28f170a912f6
Sterchois <sterchois@protonmail.com>
2018-12-05 22:28:11 +0800

    use source-map-support moudle to make node use sourcemap

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,21 @@
+/**
+ * this is for server code source map
+ */
+const fs = require('fs')
+const path = require('path')
+require('source-map-support').install({
+  retrieveSourceMap: function() {
+		return {
+			url: __filename,
+			map: fs.readFileSync(path.join(__dirname,'../../dist/server/index.map'),'utf-8'),
+      environment: 'node'
+		};
+  }
+});
+
+// remove it after check
+throw new Error('see me in trace stack!')
+
 global.config = process.environment;
 require("~/common/defineGlobalVars");
 const log = require("../common/log");
